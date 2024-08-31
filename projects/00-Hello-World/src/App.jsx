c30be9ade36b0139c028ad681e691cf31c84a17b
Addel <sigmamediacode@gmail.com>
2024-08-30 20:15:43 -0400

    carpeta componentes y comentarios

--- projects/00-Hello-World/src/App.jsx
+++ projects/00-Hello-World/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Salute } from './helloWord'
+import { Salute } from './components/helloWord'
 import './App.css'
 
 function App() {
@@ -18,7 +18,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      {/* Llamando el componente muestra hello world */}
       <Salute />
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- projects/00-Hello-World/src/components/helloWord.jsx
+++ projects/00-Hello-World/src/components/helloWord.jsx
@@ -1,3 +1,4 @@
+
 export const Salute = () => {
     return(
         <h1>Hello World</h1>
